837b0cf4f519baf41820794a6ecf097c9b911919
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-09 14:42:49 +0100

    stlyed and ready

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,13 +3,14 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    @answer = nil
-    @answer = if params[:question].downcase == 'i am going to work right now!'
-                'Great!'
-              elsif params[:question].end_with?('?')
-                'Silly question, get dressed and go to work!'
-              else
-                'I don\'t care, get dressed and go to work!'
-              end
+    @question = params[:question]
+    @answers = nil
+    @answers = if params[:question].downcase == 'i am going to work right now!'
+                 'Great!'
+               elsif params[:question].end_with?('?')
+                 'Silly question, get dressed and go to work!'
+               else
+                 'I don\'t care, get dressed and go to work!'
+               end
   end
 end
